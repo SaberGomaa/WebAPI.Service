11380dac292b354c8a45bd7a6fdaa95a80b6cddd
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-27 16:56:53 +0200

    remove virtual

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace WebAPI.Service.Models
 {
     public class Employee
     {
-        public int Id { get; set; }
+        //already Serializable
+        public int Id { get; set; } 
         public string Name { get; set; }
         public int Age { get; set; }
         public decimal Salary { get; set; }
         public int DepartmentId { get; set; }
-        public virtual Department department { get; set; }
+        [IgnoreDataMember]
+        public  Department department { get; set; }
 
     }
 }
